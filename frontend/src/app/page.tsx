00de56120e0bf7e5973bea93a70efe7c59da97c8
unknown <monhitgelsh@gmail.com>
2024-09-02 19:43:04 +0700

    first commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,8 @@
 import { CarouselLayout } from "@/components/CarouselLayout";
 
-
 export default function Home() {
   return (
-   <div className="w-full m-auto  h-[100vh]">
+   <div className="w-full m-auto h-[100vh]">
     <CarouselLayout/>
    </div>
   );
